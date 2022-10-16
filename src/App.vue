94fa813fd7daa22e0b245c0b220c6a6faf579057
MakerHu <1003964217@qq.com>
2022-10-16 13:24:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+import raadingTask1 from './assets/reading.json'
+import raadingTask2 from './assets/reading2.json'
+import listenTask3 from './assets/listen.json' 
+
+const tabPosition = ref('tab1')
+</script>
+
+<template>
+  <header>
+    <div>
+      <div style="display: flex;justify-content: center;margin-bottom: 10px;">
+        <img alt="逢考必过" class="logo" src="./assets/fkbg.png" height="150" />
+      </div>
+
+      <!-- <div class="wrapper">
+        <HelloWorld msg="逢考必过！" />
+      </div> -->
+    </div>
+    
+  </header>
+
+  <main>
+    <div style="width: 100%;text-align: center;">
+      <el-radio-group v-model="tabPosition" style="margin-bottom: 10px;">
+        <el-radio-button label="tab1">阅读task1</el-radio-button>
+        <el-radio-button label="tab2">阅读task2</el-radio-button>
+        <el-radio-button label="tab3">听力task3</el-radio-button>
+      </el-radio-group>
+    </div>
+    
+
+    <TheWelcome v-if="tabPosition == 'tab1'" :dataParam="raadingTask1" />
+    <TheWelcome v-else-if="tabPosition == 'tab2'" :dataParam="raadingTask2" />
+    <TheWelcome v-else="tabPosition == 'tab3'" :dataParam="listenTask3" />
+  </main>
+  <footer>
+    <div style="display: flex;justify-content: center;font-size: small;">©胡江浩 张星宇 黄琳棠</div>
+  </footer>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    /* display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2); */
+    display: flex;
+    justify-content: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  /* header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  } */
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,120 @@
+<script setup>
+import { ref, reactive,onMounted,onUnmounted } from 'vue'
+import { Refresh, Top, Hide, View } from '@element-plus/icons-vue'
+
+const props = defineProps({
+  dataParam: {
+    type: Array,
+    required: true
+  }
+})
+
+let data1 = reactive(props.dataParam)
+function onSelect(selectItem) {
+  selectItem.selected = !selectItem.selected
+}
+function onRestart() {
+  data1 = reactive(shuffleSelf(data1))
+  data1.forEach(element => {
+    element.choice = reactive(shuffleSelf(element.choice))
+    element.choice.forEach(item => {
+      item.selected = false
+    })
+  });
+}
+const isOpenEyes = ref(false)
+function openEyes() {
+  isOpenEyes.value = !isOpenEyes.value
+  data1.forEach(element => {
+    element.choice.forEach(item => {
+      item.selected = item.correct && isOpenEyes.value ? true : false
+    })
+  });
+}
+
+function shuffleSelf(array, size) {
+    var index = -1,
+        length = array.length,
+        lastIndex = length - 1;
+
+    size = size === undefined ? length : size;
+    while (++index < size) {
+        var rand = index + Math.floor( Math.random() * (lastIndex - index + 1)),
+            value = array[rand];
+
+        array[rand] = array[index];
+
+        array[index] = value;
+    }
+    array.length = size;
+    return array;
+}
+
+const backTopFlag = ref(false)//用来判断样式
+const backTop = () => {
+  let top = document.documentElement.scrollTop//获取点击时页面的滚动条纵坐标位置
+  const timeTop = setInterval(() => {
+    document.documentElement.scrollTop = top -= 100//一次减50往上滑动
+    if (top <= 0) {
+      clearInterval(timeTop)
+    }
+  }, 5)//定时调用函数使其更顺滑
+}
+const handleScroll = () => {
+  if (document.documentElement.scrollTop > 20) {
+    backTopFlag.value = true
+  } else {
+    backTopFlag.value = false
+  }
+  //往下滑超过20则显示 否则则不显示按钮
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})//监听滚动事件
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})//离开页面时移除监听事件
+
+</script>
+
+<template>
+  <div style="width: 100%;text-align: right;position: sticky;top: 10px;z-index: 999;">
+    <el-button circle :icon="Refresh" @click="onRestart()"></el-button>
+    <el-button circle :icon="isOpenEyes? View:Hide" @click="openEyes()" style="margin: 0 5px 0 5px;"></el-button>
+  </div>
+  
+  <div v-show="backTopFlag" style="width: 100%;text-align: right;position: fixed;bottom: 20px;z-index: 999;">
+    <el-button circle :icon="Top" @click="backTop()" style="margin-right: 38px;"></el-button>
+  </div>
+  
+  <el-card class="box-card" v-for="item,index in data1">
+    <template #header>
+      <div class="card-header">
+        <span>{{ (index+1) + '. ' + item.question }}</span>
+      </div>
+    </template>
+    <div v-for="o in item.choice" :key="o.key">
+      <el-button class="answer-btn" :type="o.selected ? (o.correct ? 'success':'danger'):''" :text="!o.selected" @click="onSelect(o)">{{ o.content }}</el-button>
+    </div>
+  </el-card>
+</template>
+
+<style>
+.box-card {
+  margin-top: 10px;
+}
+
+.answer-btn {
+  margin-top: 3px;
+  width: 100%;
+  word-wrap:break-word;
+  overflow-wrap: break-word;
+  white-space: normal;
+  height: auto;
+  overflow: hidden;
+  text-align: left;
+  justify-content: left;
+}
+</style>
